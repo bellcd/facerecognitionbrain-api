b708898c77a3a09a5b622ef94259b0d23e1de2f1
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-02-25 14:06:55 -0500

    sets up basic environment variables

--- server.js
+++ server.js
@@ -29,6 +29,11 @@ app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
 app.put("/image", (req, res) => image.handleImage(req, res, db));
 app.post("/imageurl", (req, res) => image.handleApiCall(req, res));
 
-app.listen(3000, () => {
-  console.log('app is running on port 3000');
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
+
+
+console.log(PORT)
